a4f01f526ce9dad29dbe6a1f72d80e544b046fcc
Nick <nbuhay@gmail.com>
2014-02-22 14:59:04 -0500

    Access to chord_list object based on url

--- test.js
+++ test.js
@@ -33,8 +33,40 @@ function serveStatic (response, cache, absPath) {
 	}
 }
 
-var server2 = http.createServer(function (request, response) {
+var server2 = http.createServer(function (req, res) {
+	if(req.url == '/a_maj') {
+		// fs.rename('dummy.txt', 'smart.txt', function(err, data) {
+		// 	if(err) {
+		// 		console.log(err);
+		// 	} else {
+		// 		console.log('Success!' + data);
+		// 	}
+		// });
 
+		fs.readFile('./chord_list.json', function(err, data) {
+			if(err) throw err;
+			var notes = JSON.parse(data.toString());
+			console.log(notes.maj.a);
+			console.log(notes);
+
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			res.end();
+		});
+	} else if(req.url == '/') {
+		console.log('Hello');
+	} else if(req.url == '/d_min') {
+		fs.readFile('./chord_list.json', function(err, data) {
+			if(err) throw err;
+			var notes = JSON.parse(data.toString());
+			console.log(notes.min.d);
+			console.log(notes);
+
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			res.end();
+		})
+	}
+}).listen(1200, function() {
+	console.log('Listening on port 1200');
 });
 
 var server1 = http.createServer(function (request, response) {
