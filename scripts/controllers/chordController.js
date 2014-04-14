36d5ba2eff1aa5962513d76b0e62d0aef71691a9
Nick <nbuhay@gmail.com>
2014-04-14 01:56:55 -0400

    modal modification in progress

--- scripts/controllers/chordController.js
+++ scripts/controllers/chordController.js
@@ -1,5 +1,9 @@
 var error = require('./errorController.js');
 var fs = require('fs');
+var nimble = require('nimble');
+var noteList = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+var nameList = ['tonic', 'supertonic', 'mediant', 'subdominant',
+				'dominant', 'supermediant', 'leadingTone'];
 
 var notFoundPath = './public/404.html';
 
@@ -32,7 +36,7 @@ exports.lookup = function (chord, quality, res) {
 
 exports.scaleLookup = function (quality, intonation, tonic, res) {
 	console.log("quality value : " + quality);
-	var scalePath = "./"+quality+"_scale.json";
+	var scalePath = "./major_scale.json";
 	// console.log(scalePath); Will need to check and make sure no error on scale path
 	fs.readFile(scalePath, function(err, data) {
 		if(err) {
@@ -49,8 +53,42 @@ exports.scaleLookup = function (quality, intonation, tonic, res) {
 			});
 		} else {
 			var scale = JSON.parse(data);
-			scale = scale[intonation][tonic];
-			var body = 	'<h1>'+(scale[0].note)+' Major</h1>';
+			switch(quality) {
+				case 'dorian':
+					var dorTonic;
+					if(noteList.indexOf(tonic) == 0) {
+						dorTonic = noteList[6];
+					} else {
+						dorTonic = noteList(noteList.indexOf(tonic)-1);
+					}
+
+					if (scale['natural'][dorTonic][1].note == tonic &&
+						scale['natural'][dorTonic][1].intonation == intonation) {
+						scale = scale['natural'][dorTonic];
+					}
+					 else if (scale['sharp'][dorTonic][1].name == tonic &&
+						scale['sharp'][dorTonic][1].intonation == intonation) {
+						scale = scale['sharp'][dorTonic];
+					} else {
+						scale = scale['flat'][dorTonic];
+					}
+					break;
+				default:
+					scale = scale[intonation][tonic];
+			}
+
+			console.log(tonic);
+			console.log(scale[0].note != tonic);
+			while(scale[0].note != tonic) {
+				scale.push(scale.shift());
+			}
+
+			for(var i = 0; i < scale.length; i++) {
+				scale[i].name = nameList[i];
+			}
+
+			console.log(scale);
+			var body = 	'<h1>'+tonic+' '+quality+'</h1>';
 			for(var i = 0; i < scale.length; i++) {
 				body+='<p>'+scale[i].name+': '+scale[i].note;
 				if(scale[i].intonation) {
@@ -65,8 +103,3 @@ exports.scaleLookup = function (quality, intonation, tonic, res) {
 		}
 	});
 }
-
-// exports.calcTriad = function(scale) {
-// 	var triad = {scale[0], scale[2], scale[4]};
-// 	return triad;
-// }
