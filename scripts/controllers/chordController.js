d394ef818a6d6055196f638a10edc423f42d4203
Nick <nbuhay@gmail.com>
2014-04-26 17:21:49 -0400

    refactored chord controller, included all cases

--- scripts/controllers/chordController.js
+++ scripts/controllers/chordController.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var nimble = require('nimble');
 var noteList = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
 var nameList = ['tonic', 'supertonic', 'mediant', 'subdominant',
-				'dominant', 'supermediant', 'leadingTone'];
+'dominant', 'supermediant', 'leadingTone'];
 
 var notFoundPath = './public/404.html';
 
@@ -34,6 +34,16 @@ exports.lookup = function (chord, quality, res) {
 	});
 }
 
+var findCorrectModeTonic = function(tonic, modeDegree) {
+	console.log("in findCorrectModeTonic");
+	for(var i = noteList.length-modeDegree, j = 0; i < noteList.length; i++, j++) {
+		if(noteList.indexOf(tonic) == j) {
+			return noteList[i];
+		}
+	}
+	return noteList(noteList.indexOf(tonic)-modeDegree);
+}
+
 exports.scaleLookup = function (quality, intonation, tonic, res) {
 	console.log("quality value : " + quality);
 	var scalePath = "./major_scale.json";
@@ -53,110 +63,61 @@ exports.scaleLookup = function (quality, intonation, tonic, res) {
 			});
 		} else {
 			var scale = JSON.parse(data);
+			var modeTonic;
+			var modeDegree;  // based off array indexing i.e. dorian is second mode thus modeDegree is 2
+			
 			// find tonic of major scale where the second degree
 			// of the major scale is equal to passed tonic
 			switch(quality) {
 				case 'dorian':
-					var dorTonic;
 					// if the passed second degree is A, then theory tells
 					// that the tonic will be G (the last element of noteList)
-					if(noteList.indexOf(tonic) == 0) {
-						dorTonic = noteList[6];
-					} else {
-						dorTonic = noteList(noteList.indexOf(tonic)-1);
-					}
-
-					if (scale['natural'][dorTonic][1].note == tonic &&
-						scale['natural'][dorTonic][1].intonation == intonation) {
-						scale = scale['natural'][dorTonic];
-					}
-					 else if (scale['sharp'][dorTonic][1].name == tonic &&
-						scale['sharp'][dorTonic][1].intonation == intonation) {
-						scale = scale['sharp'][dorTonic];
-					} else {
-						scale = scale['flat'][dorTonic];
-					}
+					modeDegree = 1;
 					break;
 				case 'phrygian':
-					var phryTonic;
-					if(noteList.indexOf(tonic == 0) {
-						phryTonic = noteList[5];
-					} else if(noteList.indexOf(tonic == 1) {
-						phryTonic = noteList[6];
-					} else {
-						phryTonic = noteList(noteList.indexOf(tonic)-2);
-					}
-
-					if (scale['natural'][phryTonic][2].note == tonic &&
-						scale['natural'][phryTonic][2].intonation == intonation) {
-						scale = scale['natural'][dorTonic];
-					}
-					 else if (scale['sharp'][phryTonic][2].name == tonic &&
-						scale['sharp'][phryTonic][2].intonation == intonation) {
-						scale = scale['sharp'][phryTonic];
-					} else {
-						scale = scale['flat'][phryTonic];
-					}
+					modeDegree = 2;
 					break;
 				case 'lydian':
-					var lydTonic;
-					if(noteList.indexOf(tonic == 0) {
-						lydTonic = noteList[4];
-					} else if(noteList.indexOf(tonic == 1) {
-						lydTonic = noteList[5];
-					} else if (noteList.indexOf(tonic == 2) {
-						lydTonic = noteList[6];
-					} else {
-						lydTonic = noteList(noteList.indexOf(tonic)-3);
-					}
-
-					if (scale['natural'][lydTonic][3].note == tonic &&
-						scale['natural'][lydTonic][3].intonation == intonation) {
-						scale = scale['natural'][lydTonic];
-					}
-					 else if (scale['sharp'][lydTonic][3].name == tonic &&
-						scale['sharp'][lydTonic][3].intonation == intonation) {
-						scale = scale['sharp'][lydTonic];
-					} else {
-						scale = scale['flat'][lydTonic];
-					}
+					modeDegree = 3;
 					break;
 				case 'mixolydian':
-					var mixTonic;
-					if(noteList.indexOf(tonic == 0)) {
-						mixTonic = noteList[3];
-					} else if(noteList.indexOf(tonic == 1)) {
-						mixTonic = noteList[4];
-					} else if(noteList.indexOf(tonic == 2)) {
-						mixTonic = noteList[5];
-					} else if(noteList.indexOf(tonic == 3)) {
-						mixTonic = noteList[6];
-					} else {
-						mixTonic = noteList(noteList.indexOf(tonic)-4);
-					}
-
-					if (scale['natural'][lydTonic][4].note == tonic &&
-						scale['natural'][lydTonic][4].intonation == intonation) {
-						scale = scale['natural'][lydTonic];
-					}
-					 else if (scale['sharp'][lydTonic][4].name == tonic &&
-						scale['sharp'][lydTonic][4].intonation == intonation) {
-						scale = scale['sharp'][lydTonic];
-					} else {
-						scale = scale['flat'][lydTonic];
-					}
+					modeDegree = 4;
+					break;
+				case 'aeolian':
+					modeDegree = 6;
+					break;
+				case 'locrian':
+					modeDegree = 7;
 				default:
-					scale = scale[intonation][tonic];
+					// NEED DEFAULT
+			}
+	
+			modeTonic = findCorrectModeTonic(tonic, modeDegree);
+
+			console.log("modeTonic: " + modeTonic);
+			console.log("modeDegree: "  + modeDegree);
+
+			if (scale['natural'][modeTonic][modeDegree].note == tonic &&
+				scale['natural'][modeTonic][modeDegree].intonation == intonation) {
+					scale = scale['natural'][modeTonic];
+			} else if (scale['sharp'][modeTonic][modeDegree].name == tonic &&
+				scale['sharp'][modeTonic][modeDegree].intonation == intonation) {
+					scale = scale['sharp'][modeTonic];
+			} else {
+				scale = scale['flat'][modeTonic];
 			}
 
+			// append incorrect tonic from correct major scale to end
 			while(scale[0].note != tonic) {
 				scale.push(scale.shift());
 			}
 
+			// map correct names to the scale
 			for(var i = 0; i < scale.length; i++) {
 				scale[i].name = nameList[i];
 			}
 
+			// Setup the model
 			var body = 	'<h1>'+tonic+' '+quality+'</h1>';
 			for(var i = 0; i < scale.length; i++) {
 				body+='<p>'+scale[i].name+': '+scale[i].note;
@@ -166,6 +127,7 @@ exports.scaleLookup = function (quality, intonation, tonic, res) {
 					body+='</p><br>'
 				}
 			}
+			// return model
 			res.setHeader('Content-Length', Buffer.byteLength(body));
 			res.writeHead(200, {'Content-Type': 'text/html'});
 			res.end(body);
